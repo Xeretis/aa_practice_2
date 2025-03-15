4c5be8279c4f81f09d23bad91e57b363ec968373
xeretis <70800211+Xeretis@users.noreply.github.com>
2025-03-15 16:42:35 +0100

    Edit

--- script.js
+++ script.js
@@ -2,17 +2,7 @@ const container = document.getElementById("container");
 
 let data = [];
 
-// Api kulcs: api-key-12345
-fetch("https://aa-api.bluemin.de/todos", {
-    headers: {
-        "X-API-Key": "api-key-12345",
-    },
-}).then((res) =>
-    res.json().then((resData) => {
-        data = resData;
-        renderData();
-    })
-);
+updateAndRender();
 
 const urlap = document.getElementById("urlap");
 
@@ -48,13 +38,107 @@ urlap.addEventListener("submit", (e) => {
     );
 });
 
+function updateAndRender() {
+    // Api kulcs: api-key-12345
+    fetch("https://aa-api.bluemin.de/todos", {
+        headers: {
+            "X-API-Key": "api-key-12345",
+        },
+    }).then((res) =>
+        res.json().then((resData) => {
+            data = resData;
+            renderData();
+        })
+    );
+}
+
 function renderData() {
     container.innerHTML = "";
     for (let i = 0; i < data.length; i++) {
         const card = document.createElement("div");
-
         card.classList.add("card");
-        card.textContent = data[i].title;
+
+        const checkbox = document.createElement("input");
+        checkbox.type = "checkbox";
+        checkbox.checked = data[i].isCompleted;
+        card.appendChild(checkbox);
+
+        checkbox.addEventListener("change", (e) => {
+            fetch(`https://aa-api.bluemin.de/todos/${data[i].id}`, {
+                method: "PATCH",
+                headers: {
+                    "X-API-Key": "api-key-12345",
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    isCompleted: checkbox.checked,
+                }),
+            }).then(() => {
+                updateAndRender();
+            });
+        });
+
+        const content = document.createElement("div");
+
+        const text = document.createElement("span");
+        text.innerText = data[i].title;
+        content.appendChild(text);
+
+        const button = document.createElement("button");
+        button.innerText = "Edit";
+        button.style.marginLeft = "5px";
+        content.appendChild(button);
+
+        button.addEventListener("click", () => {
+            content.replaceChildren();
+
+            const input = document.createElement("input");
+            input.value = data[i].title;
+            input.type = "text";
+            input.classList.add("input");
+            content.appendChild(input);
+
+            const confirmButton = document.createElement("button");
+            confirmButton.innerText = "Confirm";
+            content.appendChild(confirmButton);
+
+            confirmButton.addEventListener("click", () => {
+                fetch(`https://aa-api.bluemin.de/todos/${data[i].id}`, {
+                    method: "PATCH",
+                    headers: {
+                        "X-API-Key": "api-key-12345",
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify({
+                        title: input.value,
+                    }),
+                }).then(() => {
+                    updateAndRender();
+                });
+            });
+        });
+
+        card.appendChild(content);
+
+        const date = document.createElement("input");
+        date.type = "date";
+        date.valueAsDate = new Date(data[i].dueDate);
+        date.classList.add("input");
+        date.addEventListener("change", () => {
+            fetch(`https://aa-api.bluemin.de/todos/${data[i].id}`, {
+                method: "PATCH",
+                headers: {
+                    "X-API-Key": "api-key-12345",
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    dueDate: date.valueAsDate,
+                }),
+            }).then(() => {
+                updateAndRender();
+            });
+        });
+        card.appendChild(date);
 
         container.appendChild(card);
     }
